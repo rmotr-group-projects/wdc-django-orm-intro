c90cdffefbe624bc612e7842bf8ce7118267ef79
theonlysam <theonlymarcsmith@gmail.com>
2019-04-03 01:06:58 +0000

    completed orm exercises

--- django_orm_intro/artists/orm_exercises.py
+++ django_orm_intro/artists/orm_exercises.py
@@ -3,11 +3,11 @@ from artists.models import Artist, Song
 
 def task_1_artists_filter_by_popularity():
     """Should return all artists that have more than 80 popularity"""
-    return Artist.objects.filter(popularity__gte=80)
+    return Artist.objects.filter(popularity__gt=80)
 
 def task_2_artists_get_by_artistic_name():
     """Should return the artist which artistic name is Jimi Hendrix"""
-    return Artist.objects.filter(artistic_name='Jimi Hendrix')
+    return Artist.objects.get(artistic_name='Jimi Hendrix')
 
 
 def task_3_songs_delete():
@@ -18,19 +18,20 @@ def task_3_songs_delete():
 
 def task_4_artists_create_song():
     """Should create a new song for B.B. King artist"""
-    Song.objects.create(artist_id=100,title="BB's Song", album_name="B.B. King Hits")
+    artist = Artist.objects.get(artistic_name='B.B. King')
+    Song.objects.create(artist_id=artist.id,title="BB's Song", album_name="B.B. King Hits")
 
 
 def task_5_artists_order_by_popularity():
     """Should return all artists ordered by popularity"""
-    return Artist.objects.all().order_by(popularity)
+    return Artist.objects.all().order_by('popularity')
 
 
 def task_6_song_edit_album():
     """Should take the song with title 'Superstition' and update its album name with any other name"""
-    album_update = Song.objects.filter(title='Superstition')
-    album_update.album = "any other name"
-    album_update.save
+    album_update = Song.objects.get(title='Superstition')
+    album_update.album_name = "any other name"
+    album_update.save()
 
 
 def task_7_song_counter():
