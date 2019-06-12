fc6125a105cd779bb3b2d59c2d87ecfc828f42cf
tjking24 <thomas.king24@hotmail.co.uk>
2019-06-12 13:38:26 +0000

    Passed all tests

--- django_orm_intro/artists/orm_exercises.py
+++ django_orm_intro/artists/orm_exercises.py
@@ -2,34 +2,36 @@ from artists.models import Artist, Song
 
 
 def task_1_artists_filter_by_popularity():
-    """Should return all artists that have more than 80 popularity"""
-    pass
+    return Artist.objects.filter(popularity__gt=80)
 
 def task_2_artists_get_by_artistic_name():
-    """Should return the artist which artistic name is Jimi Hendrix"""
-    pass
+    return Artist.objects.get(artistic_name='Jimi Hendrix')
 
 
 def task_3_songs_delete():
-    """Should delete all songs that contain any letter 't' in its title"""
-    pass
-
-
+    for song in Song.objects.all():
+        if 't' in song.title.lower():
+            song.delete()
+    #Song.objects.filter(title__icontains='t').delete()
+    
+    
 def task_4_artists_create_song():
-    """Should create a new song for B.B. King artist"""
-    pass
-
-
+    artist = Artist.objects.get(artistic_name = 'B.B. King')
+    new_song = Song(artist_id=artist.id, title='Hello World', album_name='Python')
+    new_song.save()
+    #.create()
+    
 def task_5_artists_order_by_popularity():
-    """Should return all artists ordered by popularity"""
-    pass
+    return Artist.objects.all().order_by('popularity')
 
 
 def task_6_song_edit_album():
-    """Should take the song with title 'Superstition' and update its album name with any other name"""
-    pass
-
-
+    song = Song.objects.get(title='Superstition')
+    song.album_name = 'changed'
+    song.save()
+    
 def task_7_song_counter():
-    """Should return the amount of songs stored in the database"""
-    pass
+    return len(Song.objects.all())
+    #Song.objects.count()
+
+
